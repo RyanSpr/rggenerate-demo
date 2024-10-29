82f9b0baa28596f2c6f5de808bc81810e648f2eb
Ryan Springett <ryan.springett@red-gate.com>
2024-10-29 16:38:13 +0000

    working version!

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 rggenerate ^
-populate --source-connection-string "mssql://WIN2016/AdventureWorksLT2016?trusted_connection=yes" ^
-         --target-connection-string "mssql://WIN2016/AdventureWorksLT2016Copy?trusted_connection=yes" ^
-         --generation-file C:\Users\redgate\Desktop\Generate\generation-config.json.json
+populate --source-connection-string "mssql://WIN2016/AdventureWorksLT2016?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server" ^
+         --target-connection-string "mssql://WIN2016/AdventureWorksLT2016Copy?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server" ^
+         --generation-file C:\Users\redgate\Desktop\Generate\generation-config.json --agree-to-eula
  
