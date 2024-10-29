6c946e6576efec5f7ee068cfa8db034af3cc46b4
Ryan Springett <ryan.springett@red-gate.com>
2024-10-29 16:33:50 +0000

    updated file loc of generation-config.json

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 rggenerate ^
 populate --source-connection-string "mssql://WIN2016/AdventureWorksLT2016?trusted_connection=yes" ^
          --target-connection-string "mssql://WIN2016/AdventureWorksLT2016Copy?trusted_connection=yes" ^
-         --generation-file C:\Users\James.Hemson\EndToEndSubsetClassify\datageneration.json
+         --generation-file C:\Users\redgate\Desktop\Generate\generation-config.json.json
  
