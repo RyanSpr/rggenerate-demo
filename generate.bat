1f319e31d45b42cb048dca66edf22557dc7dc97a
Ryan Springett <ryan.springett@red-gate.com>
2024-10-31 16:57:35 -0700

    add pause command to batch file

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 rggenerate ^
 populate --source-connection-string "mssql://WIN2016/AdventureWorksLT2016?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server" ^
          --target-connection-string "mssql://WIN2016/AdventureWorksLT2016Copy?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server" ^
-         --generation-file C:\Users\redgate\Desktop\Generate\generation-config.json --agree-to-eula
+         --generation-file C:\Users\redgate\Desktop\Generate\generation-config.json --agree-to-eula & pause
  
